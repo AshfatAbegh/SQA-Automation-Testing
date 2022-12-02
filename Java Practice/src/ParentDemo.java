c6a13e578f6523dc3c305ef53a38b494499e08ad
AshfatAbegh <mdabegh50@gmail.com>
2022-12-02 17:59:37 +0600

    Java Practice Successful

--- Java Practice/src/ParentDemo.java
+++ Java Practice/src/ParentDemo.java
@@ -2,6 +2,15 @@ public class ParentDemo {
 	
 	String name = "Araf"; 
 	
+	public ParentDemo()
+	{
+		System.out.println("Parent class constructor");
+	}
+	
+	public void getData()
+    {
+		System.out.println("From Parent Class");
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         
